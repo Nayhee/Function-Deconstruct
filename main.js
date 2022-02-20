365f2eb8c2886af6872b0ab0a07035dd75fd8d68
Nayhee <nathantraczewski@gmail.com>
2022-02-20 14:19:31 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const sitDownAtDrumSet = () => {
+    console.log(`I sat down at my drumset.`)
+}
+const grabDrumSticks = () => {
+    console.log(`I grabbed my drum sticks.`)
+}
+const hitTheDrums = () => {
+    console.log(`I smacked the heck out of those drums.`)
+}
+const placeSticksDown = () => {
+    console.log(`I placed the sticks down.`)
+}
+const exitTheDrumSet = () => {
+    console.log(`I left the drumset.`)
+}
+
+const playDrums = () => {
+    sitDownAtDrumSet()
+    grabDrumSticks()
+    hitTheDrums()
+    placeSticksDown()
+    exitTheDrumSet()
+}
+playDrums()
